19b20d640863e16dc342c2bc89d393f52bb0878b
Vinicius Mayrink <vmayrink12@gmail.com>
2024-06-04 14:14:18 -0300

    Adding more remaps

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -7,4 +7,6 @@ vim.keymap.set("n", "<leader>yl", ':y<CR>')
 vim.keymap.set("n", "<leader><tab>", ':b#<CR>')
 vim.keymap.set("n", "<leader>w", vim.cmd.w)
 vim.keymap.set("n", "<leader>h", ':noh<CR>')
+vim.keymap.set("n", "<leader>n", '<C-w><C-w>')
+vim.keymap.set("n", "<leader>q", ':q<CR>')
 vim.opt.clipboard="unnamedplus"
