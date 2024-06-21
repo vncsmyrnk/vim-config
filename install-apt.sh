e1c9764a6698dbda65351c05d9b99a13ecff0aee
Vinicius Mayrink <vmayrink12@gmail.com>
2024-06-21 18:02:10 -0300

    Fixing space after last step

--- install-apt.sh
+++ install-apt.sh
@@ -4,6 +4,7 @@ stderr_file=.nvim-installer-error-log
 stdout_file=/dev/null
 
 apt_dependencies=(build-essential curl tar git)
+neovim_config_path=~/.config/nvim
 
 handle_error() {
   echo -e "\n"
@@ -52,7 +53,6 @@ show_done
 
 echo -e "\nApplying neovim configs..."
 {
-  neovim_config_path=~/.config/nvim
   if [ -d $neovim_config_path ]; then
     rm -rf $neovim_config_path
   fi
@@ -60,7 +60,7 @@ echo -e "\nApplying neovim configs..."
 } > $stdout_file 2> $stderr_file || handle_error "Error on applying configs"
 show_done
 
-echo -e "\033[1;32mInstallation completed.\033[0m"
+echo -e "\033[1;32m\nInstallation completed.\033[0m"
 echo -e "\033[4mNeovim\033[0m is installed and configured. You can now open neovim with \033[1mnvim\033[0m command"
 echo -e "On first run, make sure to skip the errors and execute \033[1m:PackerSync\033[0m to install dependencies"
 echo -e "\033[2mYou may need to run it a couple times and restart neovim in order to complete the installation \033[0m"
