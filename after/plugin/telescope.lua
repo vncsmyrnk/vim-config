d3dcf568ff7b329ea4171d8fc65fbbaf99171024
Vinicius Mayrink <vmayrink12@gmail.com>
2024-08-11 10:06:09 -0300

    chore: Excluding versioning of local plugins and including sort for mru in telescope buffers

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -3,5 +3,5 @@ local extensions = require('telescope').extensions
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {}) -- ripgrep is necessary (`sudo apt install ripgrep`)
 vim.keymap.set('n', '<leader>fa', extensions.live_grep_args.live_grep_args, {})
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fb', function() builtin.buffers({ sort_mru = true, ignore_current_buffer = true }) end, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
