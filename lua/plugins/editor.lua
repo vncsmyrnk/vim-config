bc409715c41004423129119e11f6256ee1bc4632
Vinicius Mayrink <vmayrink12@gmail.com>
2024-08-26 08:51:00 -0300

    chore: adding noice, pairs and which key config

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -38,8 +38,7 @@ return {
       spec = {
         {"<leader>f", group = "File find"},
         {"<leader>e", group = "File explorer"},
-        {"<leader>v", group = "Git"},
-        {"<leader>g", group = "LSP"},
+        {"<leader>g", group = "Git"},
         {"<leader>q", group = "Quit"},
 
         {"gK", desc = "LSP: Hover"},
@@ -93,7 +92,7 @@ return {
     "tpope/vim-fugitive",
     lazy = false,
     keys = {
-      {"<leader>vs", "<cmd>Git<cr>", desc = "Fugitive: Git status"}
+      {"<leader>gs", "<cmd>Git<cr>", desc = "Fugitive: Git status"}
     }
   },
 
@@ -123,6 +122,7 @@ return {
     "lukas-reineke/indent-blankline.nvim",
     main = "ibl",
     opts = {},
+    enabled = false,
   },
 
   {
@@ -148,8 +148,45 @@ return {
     end,
   },
 
+  {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    opts = {
+      lsp = {
+        override = {
+          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+          ["vim.lsp.util.stylize_markdown"] = true,
+          ["cmp.entry.get_documentation"] = true,
+        },
+      },
+      presets = {
+        bottom_search = true,
+        command_palette = true,
+        long_message_to_split = true,
+        inc_rename = false,
+        lsp_doc_border = false,
+      },
+    },
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "rcarriga/nvim-notify",
+    },
+    keys = {
+      {"<leader>snl", function() require("noice").cmd("last") end, desc = "Noice Last Message"},
+      {"<leader>snh", function() require("noice").cmd("history") end, desc = "Noice History"},
+      {"<leader>sna", function() require("noice").cmd("all") end, desc = "Noice All"},
+      {"<leader>snd", function() require("noice").cmd("dismiss") end, desc = "Dismiss All"},
+    },
+  },
+
+  {
+    "echasnovski/mini.pairs",
+    config = function()
+      require('mini.pairs').setup()
+    end,
+  },
+
   {"rhysd/git-messenger.vim"},
-  {"echasnovski/mini.pairs"},
   {"sindrets/diffview.nvim"},
   {"echasnovski/mini.nvim", version = "*"},
 }
