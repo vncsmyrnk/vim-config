26e243cc7f5a663c07749fdcdaf652ea1afa96ec
Vinicius Mayrink <vmayrink12@gmail.com>
2024-08-29 22:09:53 -0300

    chore: lualine bubbles theme

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -83,16 +83,52 @@ return {
   {
     "nvim-lualine/lualine.nvim",
     event = "VeryLazy",
-    opts = {
-      sections = {
-        lualine_c = {
-          {
-            "filename",
-            path = 1
+    config = function()
+      local colors = {
+        blue   = "#80a0ff",
+        cyan   = "#79dac8",
+        black  = "#080808",
+        white  = "#c6c6c6",
+        red    = "#ff5189",
+        violet = "#d183e8",
+        grey   = "#303030",
+      }
+
+      local bubbles_theme = {
+        normal = {
+          a = {fg = colors.black, bg = colors.violet},
+          b = {fg = colors.white, bg = colors.grey},
+          c = {fg = colors.white},
+        },
+
+        insert = {a = {fg = colors.black, bg = colors.blue}},
+        visual = {a = {fg = colors.black, bg = colors.cyan}},
+        replace = {a = {fg = colors.black, bg = colors.red}},
+
+        inactive = {
+          a = {fg = colors.white, bg = colors.black},
+          b = {fg = colors.white, bg = colors.black},
+          c = {fg = colors.white},
+        },
+      }
+
+      require("lualine").setup {
+        options = {
+          theme = bubbles_theme,
+          component_separators = "",
+          section_separators = {left = "", right = ""},
+        },
+        sections = {
+          lualine_c = {
+            {
+              "filename",
+              path = 1
+            }
           }
         }
+
       }
-    },
+    end,
   },
 
   {
