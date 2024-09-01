7a6edad0121bc7a4f988ec0488cf85793223cd13
Vinicius Mayrink <vmayrink12@gmail.com>
2024-09-01 13:49:14 -0300

    fix: adding explicit json jq format for json files

---
+++ ftplugin/json.lua
@@ -0,0 +1,3 @@
+-- https://github.com/rest-nvim/rest.nvim/issues/414#issuecomment-2308910953
+vim.bo.formatexpr = ""
+vim.bo.formatprg = "jq"
--- lua/plugins/format.lua
+++ lua/plugins/format.lua
@@ -4,6 +4,7 @@ return {
     opts = {
       formatters_by_ft = {
         lua = { "stylua" },
+        json = { "jq" }
       },
       format_on_save = {
         timeout_ms = 500,
