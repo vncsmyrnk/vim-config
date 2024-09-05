53b6df10609019a8a142f74c02112e7d752eadc5
Vinicius Mayrink <vmayrink12@gmail.com>
2024-09-05 14:31:36 -0300

    chore: php format on save exception

--- lua/plugins/format.lua
+++ lua/plugins/format.lua
@@ -6,10 +6,17 @@ return {
         lua = { "stylua" },
         json = { "jq" }
       },
-      format_on_save = {
-        timeout_ms = 500,
-        lsp_format = "fallback",
-      },
+      format_on_save = function(bufnr)
+        -- INFO: https://github.com/stevearc/conform.nvim/blob/master/doc/recipes.md
+        local ignore_filetypes = { "php" }
+        if vim.tbl_contains(ignore_filetypes, vim.bo[bufnr].filetype) then
+          return
+        end
+        return {
+          timeout_ms = 500,
+          lsp_format = "fallback"
+        }
+      end,
     },
   }
 }
