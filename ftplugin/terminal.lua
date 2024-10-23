7014eaf119db1bf763ba22b8056fb186d58883c4
Vinicius Mayrink <vmayrink12@gmail.com>
2024-10-23 18:19:09 -0300

    chore: adds window navigation keymaps

--- ftplugin/terminal.lua
+++ ftplugin/terminal.lua
@@ -3,5 +3,3 @@ vim.opt_local.relativenumber = false
 vim.opt_local.signcolumn = "yes:1"
 
 vim.cmd("startinsert")
-
-vim.api.nvim_set_keymap("t", "<Esc>", [[<C-\><C-n>]], { noremap = true, silent = true })
--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -23,6 +23,23 @@ vim.keymap.set("n", "<C-Down>", "<cmd>resize -2<cr>", { desc = "Decrease Window 
 vim.keymap.set("n", "<C-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease Window Width" })
 vim.keymap.set("n", "<C-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase Window Width" })
 
+-- Window
+vim.keymap.set("t", "<A-h>", [[<C-\><C-N><C-w>h]], { desc = "Go to window on the right" })
+vim.keymap.set("t", "<A-j>", [[<C-\><C-N><C-w>j]], { desc = "Go to window below" })
+vim.keymap.set("t", "<A-k>", [[<C-\><C-N><C-w>k]], { desc = "Go to window above" })
+vim.keymap.set("t", "<A-l>", [[<C-\><C-N><C-w>l]], { desc = "Go to window on the left" })
+vim.keymap.set("t", "<A-t>", [[<C-\><C-N><C-w>t]], { desc = "Go to window on the top left" })
+vim.keymap.set("t", "<A-b>", [[<C-\><C-N><C-w>b]], { desc = "Go to window on the right bottom" })
+vim.keymap.set("n", "<A-h>", [[<C-w>h]], { desc = "Go to window on the right" })
+vim.keymap.set("n", "<A-j>", [[<C-w>j]], { desc = "Go to window below" })
+vim.keymap.set("n", "<A-k>", [[<C-w>k]], { desc = "Go to window above" })
+vim.keymap.set("n", "<A-l>", [[<C-w>l]], { desc = "Go to window on the left" })
+vim.keymap.set("n", "<A-t>", [[<C-w>t]], { desc = "Go to window on the top left" })
+vim.keymap.set("n", "<A-b>", [[<C-w>b]], { desc = "Go to window on the right bottom" })
+
+-- Terminal mode
+vim.keymap.set("t", "<A-q>", [[<C-\><C-N>]], { desc = "Exit terminal mode" })
+
 -- Lazy
 vim.keymap.set("n", "<leader>l", "<cmd>Lazy<cr>", { desc = "Lazy" })
 
