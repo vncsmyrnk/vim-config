7040f7f0287ab497df2fed5d6a6fa04e052a2917
Vinicius Mayrink <vmayrink12@gmail.com>
2024-12-07 22:31:08 -0300

    feat: adds vim dadbod ui as a SQL client; still needs a setup and tests

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -399,6 +399,24 @@ return {
     },
   },
 
+  {
+    "kristijanhusak/vim-dadbod-ui",
+    dependencies = {
+      { "tpope/vim-dadbod", lazy = true },
+      { "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" }, lazy = true },
+    },
+    cmd = {
+      "DBUI",
+      "DBUIToggle",
+      "DBUIAddConnection",
+      "DBUIFindBuffer",
+    },
+    init = function()
+      vim.g.db_ui_use_nerd_fonts = 1
+      -- vim.g.dbs = {}
+    end,
+  },
+
   -- INFO: https://www.jetbrains.com/help/idea/exploring-http-syntax.html
   {
     "rest-nvim/rest.nvim",
