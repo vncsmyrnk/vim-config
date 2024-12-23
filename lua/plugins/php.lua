8738190bd98bdf30b0b78a8925ebe14d001c2b7f
Vinicius Mayrink <vmayrink12@gmail.com>
2024-12-23 17:34:12 -0300

    fix: adds autocmd for formatting PHP files; needs fix
    after saving the file itself becomes sus

--- lua/plugins/php.lua
+++ lua/plugins/php.lua
@@ -6,5 +6,14 @@ return {
   {
     "stephpy/vim-php-cs-fixer",
     ft = "php",
+    enabled = false,
+    config = function()
+      vim.api.nvim_create_autocmd("BufWritePost", {
+        pattern = "*.php",
+        callback = function()
+          vim.cmd([[silent! call PhpCsFixerFixFile()]])
+        end,
+      })
+    end,
   },
 }
